11cee052c12336bf6ab70cdee643fe2e6a8fe445
jiajie <jiajie@momenta.ai>
2016-12-17 21:52:39 +0800

    version 0.08

--- PyLuaTblParser.py
+++ PyLuaTblParser.py
@@ -9,43 +9,41 @@ class PyLuaTblParser():
 5. loadDict(self, d)   读取dict中的数据，存入类中，只处理数字和字符串两种类型的key，其他类型的key直接忽略；
 6. dumpDict(self)  返回一个dict，包含类中的数据
     '''
-    dict = {}
-    ori_str=""""""
-    cur_valid=""
-    quotations = []
-    quotations_index=-1
-    # quotation_2 = []
 
     def __init__(self):
         self.dict = {}
+        self.quotations = []
+        self.cur_valid = ""
 
     def load(self, s):
         # remove useless spaces
         self.removeSpace(s)
-        s=self.cur_valid
-        print s
+        s = self.cur_valid
         # update quotations
-        self.scanQuotations(s) # update quotations
-        # remove sensetive char in quotations
+        self.scanQuotations(s)  # update quotations
+        # remove sensitive char in quotations
         for pai in self.quotations:
-            s = s[:pai[0]] + s[pai[0]:pai[1]].replace(';', '_').replace(',','_').replace('=','_').replace('{','_').replace('}','_').replace('[','_').replace(']','_') + s[pai[1]:]
-        print s
+            s = s[:pai[0]] + s[pai[0]:pai[1]].replace(',', '_').replace('=', '_').replace('{', '_').replace('}', '_') \
+                + s[pai[1]:]
         # get all valid brackets
         brackets_stack = []
         brackets = []
-        i=0
-        while i<len(s):
+        i = 0
+        while i < len(s):
             if s[i] == '{':
                 brackets_stack.append(i)
             elif s[i] == '}':
                 brackets.append((brackets_stack[-1], i))
                 brackets_stack.pop()
-            i+=1
+            i += 1
         if len(brackets_stack):
             raise Exception('lua table string format Error on {}')
 
         # Step 3: iterate brackets
         cur_res = {}
+        work_dict = {}
+        for i in xrange(0, len(brackets)):
+            work_dict[brackets[i][0]] = False
         for i in xrange(0, len(brackets)):
             cur_bracket = brackets[i]
             # reverse scan for smaller pre bracket: get the first sub bracket of current bracket
@@ -58,11 +56,19 @@ class PyLuaTblParser():
             # load bracket keys
             pj_keys = []
             while j != i:
-                pj_keys.append(brackets[j][0])
+                if work_dict[brackets[j][0]] is False:
+                    pj_keys.append(brackets[j][0])
+                    work_dict[brackets[j][0]] = True
                 j += 1
             # deal with current bracket
             is_list = True
             content = s[cur_bracket[0] + 1:cur_bracket[1]]  # get content
+            content_quotations = []
+            for pp in self.quotations:
+                if pp[0] >= cur_bracket[0] and pp[1] < cur_bracket[1]:
+                    content_quotations.append(pp)
+            self.quotations = list(set(self.quotations) - set(content_quotations))
+            quo_index = 0
             ls = content.split(',')  # split with separator
             for l in ls:
                 if l.find('=') > -1:
@@ -82,7 +88,7 @@ class PyLuaTblParser():
                         rls.append(b)
                     else:
                         if l == '':
-                            if ls.index(l)!=len(ls)-1:
+                            if ls.index(l) != len(ls) - 1:
                                 raise Exception('empty value after split')
                             continue
                         elif len(l) > 1 and l[0] == '{' and l[-1] == '}':
@@ -90,7 +96,9 @@ class PyLuaTblParser():
                             del cur_res[pj_keys[pj_i]]
                             pj_i += 1
                         elif self.validStr(l):
-                            rls.append(l[1:-1])
+                            ori = self.cur_valid[content_quotations[quo_index][0] + 1:content_quotations[quo_index][1]]
+                            rls.append(ori)
+                            quo_index += 1
                         else:
                             rls.append(eval(l))
             else:
@@ -112,14 +120,30 @@ class PyLuaTblParser():
                             if l == '':
                                 continue
                             elif self.validStr(l):
-                                rls[index] = l[1:-1]
+                                ori = self.cur_valid[
+                                      content_quotations[quo_index][0] + 1:content_quotations[quo_index][1]]
+                                rls[index] = ori
+                                quo_index += 1
                             else:
                                 rls[index] = eval(l)
                         index += 1
                     else:
                         lk = l.split('=')
                         assert len(lk) == 2
-                        cur_key=self.validKey(lk[0])
+                        if len(lk[0]) > 2 and lk[0][0] == '[' and lk[0][-1] == ']':
+                            in_str = lk[0][1:-1]
+                            if self.validStr(in_str):
+                                ori = self.cur_valid[
+                                      content_quotations[quo_index][0] + 1:content_quotations[quo_index][1]]
+                                cur_key = ori
+                                quo_index += 1
+                            else:
+                                cur_key = eval(in_str)
+                        elif lk[0].find(' ') != -1:
+                            raise Exception("error key")
+                        else:
+                            cur_key = lk[0]
+
                         if lk[1] == 'true':
                             b = True
                             rls[cur_key] = b
@@ -135,14 +159,16 @@ class PyLuaTblParser():
                             pj_i += 1
                         else:
                             if self.validStr(lk[1]):
-                                self.quotations_index+=1
-                                rls[cur_key] = self.cur_valid[self.quotations[self.quotations_index][0]:self.quotations[self.quotations_index][1]+1]
+                                ori = self.cur_valid[
+                                      content_quotations[quo_index][0] + 1:content_quotations[quo_index][1]]
+                                rls[cur_key] = ori
+                                quo_index += 1
                             else:
                                 rls[cur_key] = eval(lk[1])
             cur_res[cur_bracket[0]] = rls
             # replace separators
-            s=s[:cur_bracket[0]]+s[cur_bracket[0]:cur_bracket[1]].replace(',','_').replace('=','_')+s[cur_bracket[1]:]
-            print s
+            s = s[:cur_bracket[0]] + s[cur_bracket[0]:cur_bracket[1]].replace(',', '_').replace('=', '_') \
+                + s[cur_bracket[1]:]
 
         for key in cur_res:
             self.dict = cur_res[key]
@@ -155,45 +181,42 @@ class PyLuaTblParser():
         self.load(open(f).read())
 
     def dumpLuaTable(self, f):
-        fw=open(f,'w')
-        # self.dict
+        ss = self.genStr(self.dict)
+        fw = open(f, 'wb')
+        fw.write(ss)
+        fw.close()
 
     def loadDict(self, d):
-        self.dict = eval(d)
-        pass
+        self.dict = eval(str(d))
 
     def dumpDict(self):
         return eval(str(self.dict))
 
     # my functions
-    def mySplit(self, s):
-        # scan for quotations
-        quotation_1 = []
-        quotation_2 = []
-        quotation_stack_1 = []
-        quotation_stack_2 = []
-        for i in xrange(0, len(s)):
-            if s[i] == '\"':
-                if len(quotation_stack_2) == 1:
-                    quotation_2.append((quotation_stack_2[-1], i))
-                    quotation_stack_2.pop()
-                else:
-                    quotation_stack_2.append(i)
-            elif s[i] == '\'':
-                if len(quotation_stack_1) == 1:
-                    quotation_1.append((quotation_stack_1[-1], i))
-                    quotation_stack_1.pop()
-                else:
-                    quotation_stack_1.append(i)
-        if len(quotation_stack_1) or len(quotation_stack_2):
-            raise Exception('lua table string format Error on ""')
-        xstr = s
-        for p in quotation_1:
-            xstr = xstr[:p[0]] + xstr[p[0]:p[1] + 1].replace(',', '_').replace(';', '_') + xstr[p[1] + 1:]
-        for p in quotation_2:
-            xstr = xstr[:p[0]] + xstr[p[0]:p[1] + 1].replace(',', '_').replace(';', '_') + xstr[p[1] + 1:]
-        xstr = xstr.replace(';', ',')
-        return xstr.split(',')
+    def genStr(self, d):
+        if isinstance(d, dict):
+            res_str = '{'
+            for k in d:
+                res_str += '[' + self.genStr(k) + ']=' + self.genStr(d[k]) + ','
+            res_str += '}'
+            return res_str
+        elif isinstance(d, list):
+            res_str = '{'
+            for k in d:
+                res_str += self.genStr(k) + ','
+            res_str += '}'
+            return res_str
+        elif isinstance(d, type(None)):
+            return 'nil'
+        elif isinstance(d, float) or isinstance(d, int):
+            return str(d)
+        elif isinstance(d, bool):
+            if d == False:
+                return 'false'
+            else:
+                return 'true'
+        else:
+            return repr(d)
 
     def validStr(self, s):
         if len(s) < 2:
@@ -205,57 +228,35 @@ class PyLuaTblParser():
         else:
             return False
 
-    def validKey(self, s):
-        if s[0] == '[' and s[-1] == ']':
-            in_str=s[1:-1].strip()
-            if in_str.find(']') > -1 or in_str.find('[')>-1:
-                return False
-            else:
-                return True
-        return True
-        # if len(s) < 2:
-        # return False
-        # if(s[0]=='\"' and s[-1] == '\"'):
-        # if s[1:-1].find('\"') > -1:
-        #         return False
-        #     else:
-        #         return True
-        # elif(s[0]=='\'' and  s[-1]!='\'' ):
-        #     if s[1:-1].find('\'') > -1:
-        #         return False
-        #     else:
-        #         return True
-        # else:
-        #     return False
-    def removeSpace(self,s):
+    def removeSpace(self, s):
         # step 1: scan for quotations
         self.scanQuotations(s)
         # step 2: iterate unquotation part
-        b1=0
-        b2=0
-        while b2<len(s):
-            hitp=None
+        b1 = 0
+        b2 = 0
+        while b2 < len(s):
+            hitp = None
             for pai in self.quotations:
                 if b2 in pai:
-                    hitp=pai
+                    hitp = pai
                     break
-            if hitp!=None:
-                b2=hitp[0]
+            if hitp != None:
+                b2 = hitp[0]
                 nb1 = hitp[1]
                 noquo = s[b1:b2]
                 # remove useless space of cur noquo
-                temp_valid=noquo
-                temp_valid="{".join(map(lambda x:x.strip(),temp_valid.split('{')))
-                temp_valid ="}".join(map(lambda x:x.strip(),temp_valid.split('}')))
-                temp_valid =",".join(map(lambda x:x.strip(),temp_valid.split(';'))) # replace ; to ,
-                temp_valid =",".join(map(lambda x:x.strip(),temp_valid.split(',')))
-                temp_valid ="=".join(map(lambda x:x.strip(),temp_valid.split('=')))
+                temp_valid = noquo
+                temp_valid = "{".join(map(lambda x: x.strip(), temp_valid.split('{')))
+                temp_valid = "}".join(map(lambda x: x.strip(), temp_valid.split('}')))
+                temp_valid = ",".join(map(lambda x: x.strip(), temp_valid.split(';')))  # replace ; to ,
+                temp_valid = ",".join(map(lambda x: x.strip(), temp_valid.split(',')))
+                temp_valid = "=".join(map(lambda x: x.strip(), temp_valid.split('=')))
                 temp_valid = "[".join(map(lambda x: x.strip(), temp_valid.split('[')))
                 temp_valid = "]".join(map(lambda x: x.strip(), temp_valid.split(']')))
-                self.cur_valid+=temp_valid+s[b2:nb1]
-                b1=nb1
-                b2=b1
-            b2+=1
+                self.cur_valid += temp_valid + s[b2:nb1]
+                b1 = nb1
+                b2 = b1
+            b2 += 1
         # last noquo
         noquo = s[b1:b2]
         # remove useless space of cur noquo
@@ -268,9 +269,8 @@ class PyLuaTblParser():
         temp_valid = "[".join(map(lambda x: x.strip(), temp_valid.split('[')))
         temp_valid = "]".join(map(lambda x: x.strip(), temp_valid.split(']')))
         self.cur_valid += temp_valid
-        self.ori_str=self.cur_valid
 
-    def scanQuotations(self,s):
+    def scanQuotations(self, s):
         quotation_1 = []
         quotation_2 = []
         quotation_stack_1 = []
@@ -278,7 +278,7 @@ class PyLuaTblParser():
         for i in xrange(0, len(s)):
             if s[i] == '\"':
                 if len(quotation_stack_2) == 1:
-                    if len(quotation_stack_1) ==0:
+                    if len(quotation_stack_1) == 0:
                         quotation_2.append((quotation_stack_2[-1], i))
                         quotation_stack_2.pop()
                     elif len(quotation_stack_1) > 0 and quotation_stack_1[-1] > quotation_stack_2[-1]:
@@ -291,7 +291,7 @@ class PyLuaTblParser():
                     quotation_stack_2.append(i)
             elif s[i] == '\'':
                 if len(quotation_stack_1) == 1:
-                    if len(quotation_stack_2) ==0:
+                    if len(quotation_stack_2) == 0:
                         quotation_1.append((quotation_stack_1[-1], i))
                         quotation_stack_1.pop()
                     elif len(quotation_stack_2) > 0 and quotation_stack_2[-1] > quotation_stack_1[-1]:
@@ -304,4 +304,4 @@ class PyLuaTblParser():
                     quotation_stack_1.append(i)
         if len(quotation_stack_1) or len(quotation_stack_2):
             raise Exception('lua table string format Error on quotations')
-        self.quotations=quotation_1+quotation_2
+        self.quotations = quotation_1 + quotation_2
--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 from PyLuaTblParser import PyLuaTblParser
 
+a1 = PyLuaTblParser()
+a2 = PyLuaTblParser()
+a3 = PyLuaTblParser()
+
+test_str = '{array = {65,23,5,},dict = {mixed = {43,54.33,false,9,string = "value",},array = {3,6,4,},string = "value",},}'
+a1.load(test_str)
+d1 = a1.dumpDict()
+file_path='lua.tbl'
+print d1
+a2.loadDict(d1)
+a2.dumpLuaTable(file_path)
+a3.loadLuaTable(file_path)
+
+d3 = a3.dumpDict()
+print d3
+
 # a1 = PyLuaTblParser()
 # a2 = PyLuaTblParser()
 # a3 = PyLuaTblParser()
@@ -28,11 +44,12 @@ from PyLuaTblParser import PyLuaTblParser
 # # a1.loadDict(d1)
 # print d1
 
-a1 = PyLuaTblParser()
-test_str = """  {  array    = {  65,23,5,  },  di  ct = {7,8,false,string="va ='lue"}   ,}   """
-a1.load(test_str)
-d1 = a1.dumpDict()
-print d1
+# a1 = PyLuaTblParser()
+# test_str = """{array = {65,23,5,},dict = {mixed = {43,54.33,false,9,string = "value",},array = {3,6,4,},string = "value",},}"""
+# # test_str = """  {  {  65,23,5,  },   {7,8,false,"va ='lue"}   ,}   """
+# a1.load(test_str)
+# d1 = a1.dumpDict()
+# print d1
 # a1.removeSpace(test_str)
 # print a1.cur_valid
 # a='123'
